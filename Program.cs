642254d5f35fce8e5ae60e88c9628101a5e813df
jalbert90 <jalbert90@gmail.com>
2020-11-29 14:55:55 -0500

    Before applying async

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
 
 namespace testingAsync
 {
@@ -6,7 +8,60 @@ namespace testingAsync
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ExecuteSync();
+        }
+
+        private static void ExecuteSync()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            RunDownloadSync();
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            Console.WriteLine("Total execution time: " + elapsedMs);
+        }
+
+        private static List<string> PrepData()
+        {
+            List<string> output = new List<string>();
+
+            output.Add("https://www.google.com");
+            output.Add("https://www.yahoo.com");
+            output.Add("https://www.microsoft.com");
+            output.Add("https://www.cnn.com");
+            output.Add("https://www.codeproject.com");
+            output.Add("https://www.stackoverflow.com");
+
+            return output;
+        }
+
+        private static void RunDownloadSync()
+        {
+            List<string> websites = PrepData();
+
+            foreach (string site in websites)
+            {
+                WebsiteDataModel results = DownloadWebsite(site);
+                ReportWebsiteInfo(results);
+            }
+        }
+
+        private static WebsiteDataModel DownloadWebsite(string websiteURL)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+
+            output.WebsiteURL = websiteURL;
+            output.WebsiteData = client.DownloadString(websiteURL);
+
+            return output;
+        }
+
+        private static void ReportWebsiteInfo(WebsiteDataModel data)
+        {
+            Console.WriteLine(data.WebsiteURL + " downloaded: " + data.WebsiteData.Length + "characters long.");
         }
     }
 }
---
+++ WebsiteDataModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace testingAsync
+{
+	public class WebsiteDataModel
+    {
+        public string WebsiteURL { get; set; } = "";
+        public string WebsiteData { get; set; } = "";
+    }
+}
